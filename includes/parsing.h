c6a50a969b75b0ee2333d87bc04ad5c58767858d
Mohamed El Ghali Tagemouati <mtagemou@e1r9p8.1337.ma>
2023-02-02 09:30:44 +0100

    fix function not reconized

--- includes/parsing.h
+++ includes/parsing.h
@@ -65,5 +65,13 @@ void	tokenise_heredoc(t_token *token);
 int		is_operator(char c);
 t_token	*word_spliting(t_token *token);
 void	free_cmd(t_cmd *head);
+void	close_fds(t_cmd *cmd);
+t_token	*add_cmd(t_cmd **cmds, t_token *tokens);
+int		handle_heredoc(t_cmd *cmd, char *limiter, char *file);
+t_token	*next_pipe(t_token *tokens);
+t_token	*next_pipe(t_token *tokens);
+void	handle_pipes(t_cmd *cmd, t_token *tokens);
+int	handle_redirection(t_cmd *cmd, t_token *tokens);
+void	handle_cmd(t_cmd *cmd, t_token *tokens);
 
 #endif // PARSING_H
